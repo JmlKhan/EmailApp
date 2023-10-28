fedca5339f489c47a96a662e67fd68ba6bfe7367
Jamolkhon Umarov <jmlkhon17@gmail.com>
2023-10-28 17:15:05 +0500

    callme API

---
+++ EmailApp/Controllers/BattutaController.cs
@@ -0,0 +1,30 @@
+﻿using EmailApp.Models;
+using EmailService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BattutaController : ControllerBase
+    {
+        private readonly IEmailSender _emailSender;
+
+        public BattutaController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpPost]
+        public ActionResult CallMeRequest(Callme callme)
+        {
+            var message = new Message(new string[] { "info@battutatravel.uz" }, "New Lead\n", callme.Number,
+                    callme.Name);
+
+            _emailSender.SendEmail(message);
+            return Ok();
+        }
+
+    }
+}
---
+++ EmailApp/Models/Callme.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailApp.Models
+{
+    public class Callme
+    {
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+    }
+}
